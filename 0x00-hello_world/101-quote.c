680f0f6103f88206cc104eb3ee69b20508570fda
Cr2do <credoay@gmail.com>
2022-03-11 01:57:00 +0100

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - quote
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+system("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+return (0);
+}
