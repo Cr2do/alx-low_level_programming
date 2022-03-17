670008ed46634b16fe3c47559fcf3a827ade05d5
Cr2do <credoay@gmail.com>
2022-03-17 22:19:06 +0100

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ *
+ * @number: integer param
+ *
+ * Return: returns 0
+ */
+
+int _isupper(char c)
+{
+	if(isupper(c)){
+        return 0;
+    }else{
+        return 1;
+    }
+}
