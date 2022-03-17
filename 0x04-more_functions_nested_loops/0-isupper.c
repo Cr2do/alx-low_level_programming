0417e458ce6a7cd16a21da113eb53632d193ae45
Cr2do <credoay@gmail.com>
2022-03-17 22:28:49 +0100

    up

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * print_last_digit - prints the last digit of a number
- *
- * @number: integer param
+ * _isupper
  *
  * Return: returns 0
  */
 
 int _isupper(char c)
 {
-	if(isupper(c)){
-        return 0;
-    }else{
-        return 1;
-    }
+if (isupper(c))
+{
+return (0);
+}
+else
+{
+return (1);
+}
 }
