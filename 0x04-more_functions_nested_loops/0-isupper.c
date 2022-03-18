6b7cc2a8914741ebfa46ad1276926eff792ecf39
Cr2do <credoay@gmail.com>
2022-03-18 18:02:27 +0100

    update

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,15 +5,15 @@
  *
  * @c: integer parameter c
  *
- * Return: returns value
+ * Return: returns value 0 or 1
  */
 int _isupper(int c)
 {
-if ('A' < c && 'Z' < c)
+if ('A' <= c && 'Z' <= c)
 {
 return (0);
 }
-else if ('a' < c && 'z' < c)
+else if ('a' <= c && 'z' <= c)
 {
 return (1);
 }
