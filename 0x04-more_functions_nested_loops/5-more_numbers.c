87d7a8f307017fd0f3e16e9762926abffa4402c4
Cr2do <credoay@gmail.com>
2022-03-18 21:22:37 +0100

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print 10 times number from 0 to 14
+ *
+ */
+void print_most_numbers(void)
+{
+int n = 0;
+while (n <= 10)
+{
+int i;
+for (i = 0; i <= 14 ; ++i)
+{
+_putchar(i + '0');
+}
+n++;
+}
+}
