ef80ae4ab27a494b1617ecd99fae9dca2edfadfa
Cr2do <credoay@gmail.com>
2022-03-18 18:59:14 +0100

    task3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,15 +6,9 @@
  */
 void print_numbers(void)
 {
-_putchar(0);
-_putchar(1);
-_putchar(2);
-_putchar(3);
-_putchar(4);
-_putchar(5);
-_putchar(6);
-_putchar(7);
-_putchar(8);
-_putchar(9);
+char c;
+for (c='a';c<='z';c++){
+_putchar(c);
+}
 _putchar('\n');
 }
