c31c2139ed4fb0b2868439a825ab51f70848a191
Cr2do <credoay@gmail.com>
2022-03-18 18:48:12 +0100

    multiple two integer

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul -  multiple two integer
+ *
+ * @a: integer a
+ * @b: integer b
+ *
+ * Return: returns value 0 or 1
+ */
+int mul(int a, int b)
+{
+int value;
+value = a * b;
+return (value);
+}
