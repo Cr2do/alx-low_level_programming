9a0448e4f54ab40da7080993f8dc1ac8bf268354
Cr2do <credoay@gmail.com>
2022-03-20 12:03:44 +0100

    Print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - print square
+ * @n: integer
+ */
+void print_square(int n)
+{
+int i;
+int j;
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < n; j++)
+{
+_putchar('#');
+}
+}
+}
+else
+{
+_putchar('\n');
+}
+
+}
