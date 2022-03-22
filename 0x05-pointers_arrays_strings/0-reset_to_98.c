e13b1f0e0a5580c1a282b1235b8e96bd8322a563
Cr2do <credoay@gmail.com>
2022-03-22 22:01:00 +0100

    task 5

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - return 98
+ * @n: pointer
+ */
+void reset_to_98(int *n)
+{
+int value;
+*n = 98;
+_putchar(value / 10 + '0');
+_putchar(value % 10 + '0');
+
+}
