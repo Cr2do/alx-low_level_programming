73a5c17fd374b10194f0b03fd135e7a219dd16ab
Cr2do <credoay@gmail.com>
2022-04-04 23:07:58 +0100

    what's my name C

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print name of programme
+ *
+ * @argc: argc:
+ * @argv: *argv[]:
+ * Return: Always 1 (Success)
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (1);
+}
