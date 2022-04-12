f08f603de57974e65d6181eb8b9783cd1ef221c9
Cr2do <credoay@gmail.com>
2022-04-12 19:16:57 +0100

    Task 2 and correction of task 3

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print name of programme
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
