6f51b915237bbc8288cd6aac83a365091a3a7253
Cr2do <credoay@gmail.com>
2022-04-14 21:31:26 +0100

    readme file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+
+/**
+ * print_name - print name function
+ *
+ * @name: name to print
+ * @f: function which print name got as input
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+(*f)(name);
+}
