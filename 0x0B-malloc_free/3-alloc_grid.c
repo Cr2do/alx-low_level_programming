0797f4ff12bd30c894c8744e39553b96023297a5
Cr2do <credoay@gmail.com>
2022-04-22 09:53:02 +0100

    malloc free update and advanced task

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,39 +11,40 @@
  */
 int **alloc_grid(int width, int height)
 {
-int i, j;
-int **p;
+	int i, j, a, b;
+	int **p;
 
-if (width <= 0 || height <= 0)
-{
-return (NULL);
-}
-p = malloc(sizeof(int) * (width + height));
-if (p == NULL)
-{
-return (NULL);
-free(p);
-}
-for (i = 0; i < width; i++)
-{
-p[i] = malloc(sizeof(int) * (width - 1));
-if (p[i] == NULL)
-{
-for (i--; i >= 0; i--)
-{
-free(p[i]);
-}
-free(p);
-return (NULL);
-}
-}
-for (i = 0; i < height; i++)
-{
-for (j = 0; j < width; j++)
-{
-p[i][j] = 0;
-}
-}
-return (p);
-free(p);
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		p = (int **) malloc(height * sizeof(int *));
+		/* we have to make a malloc per pointer*/
+		if (!p)
+		{
+			free(p);
+			return (NULL);
+		}
+		for (i = 0; i < height; i++)
+		{
+			p[i] = (int *) malloc(width * sizeof(int));
+			if (!p[i])
+			{
+				for (j = 0; j <= i; j++)
+					free(p[j]);
+				free(p);
+				return (NULL);
+			}
+		}
+		for (a = 0; a < height; a++)
+		{
+			for (b = 0; b < width; b++)
+			{
+				p[a][b] = 0;
+			}
+		}
+		return (p);
+	}
 }
