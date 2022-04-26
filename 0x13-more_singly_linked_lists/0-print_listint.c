0c0d8990420987f7a073f5e56d24e7bdaa25eb60
Cr2do <credoay@gmail.com>
2022-04-26 16:00:10 +0100

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,6 +3,14 @@
 size_t print_listint(const listint_t *h)
 {
 
-    
+    size_t count_number = 0;
+
+    while ( h != NULL)
+    {
+        count_number++;
+        printf("%d\n",h->n);
+        h = h->next;
+    }
+    printf("-> %lu elements",count_number);
 
 }
