3e46810b280e9efa6752fa219f00414de289c5f5
Cr2do <credoay@gmail.com>
2022-04-26 16:41:45 +0100

    task 0 done

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,16 +1,20 @@
 #include "lists.h"
 
+/**
+ * print_listint - that prints all the elements of a
+ * @h: list value
+ * Return: size_t
+ */
 size_t print_listint(const listint_t *h)
 {
 
-    size_t count_number = 0;
+	size_t count_number = 0;
 
-    while ( h != NULL)
-    {
-        count_number++;
-        printf("%d\n",h->n);
-        h = h->next;
-    }
-    printf("-> %lu elements",count_number);
-
+	while (h != NULL)
+	{
+		count_number++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (count_number);
 }
