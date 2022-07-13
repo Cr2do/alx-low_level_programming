91611ef2e2724ed1a6173fe856880acc1d8f9673
Cr2do <credoay@gmail.com>
2022-07-13 10:36:36 +0100

    Task 2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+
+/**
+ * key_index - key_index 
+ * 
+ * @key: const unsigned char * key
+ * @size: unsigned long int size
+ * Return: unsigned long int
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	size_t value = hash_djb2(key) % size;
+
+	return (value);
+	
+}
